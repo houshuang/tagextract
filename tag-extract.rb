be2f15e23a3ba280f09987ae20f8946a789db68e
Stian Håklev <shaklev@gmail.com>
2013-02-06 19:54:43 -0500

    Rewrite tag-extract as a class

--- tag-extract.rb
+++ tag-extract.rb
@@ -15,167 +15,183 @@ require 'utility-functions'
 # we have to do this because of using Ruby 1.8.7 regexp, with no lookahead.
 # also, we turn - into ___ because the \w end-of-word only accepts _ and not -
 # we put it all back when we're done
-def clean_ckeys(text)
-  text.gsubs([/\[\@(.+?)\]/, '[!!!!\1]'], ['-', '____'])
-end
-
-def restore_ckeys(text)
-  text.gsubs([/\[\!!!!(.+?)\]/, '[@\1]'], ['____', '-'])
-end
-
-# insert lines into an array with first argument being indent level, second being line content
-# default indent pattern is tabs
-def text2array(text, indent_pattern = /^(\t*)/)
-  lines = Array.new
-
-  text.each_line_with_index do |l, i|
-
-    # count tabs at front of line to get indent level
-    tabs = $1 if indent_pattern =~ l
-    level = (defined? tabs) ? tabs.size : 0
-
-    lines[i] = {:level => level, :line => l}
+class TagExtract
+  def initialize(text)
+    @text = clean_ckeys(text)
+    text2array
+    linecontext
+    process_tags
   end
 
-  return lines
-end
+  def to_taskpaper
+    text = output_taskpaper
+    return restore_ckeys(text)
+  end
 
+  def clean_ckeys(text)
+    text.gsubs([/\[\@(.+?)\]/, '[!!!!\1]'], ['-', '____'])
+  end
 
-# iterate through array and for each line, pick out "line context" (all subsequent lines at lower levels)
-def linecontext(lines, ckey_pattern = /^\[!!!!(.+?)\]/)
-  ckey = ''       # holds the current citation key
-  lcontext = Array.new
-  text = ''
+  def restore_ckeys(text)
+    text.gsubs([/\[\!!!!(.+?)\]/, '[@\1]'], ['____', '-'])
+  end
 
-  lines.each_with_index do |l, i|
-    level = l[:level]
-    line = l[:line]
+  # insert lines into an array with first argument being indent level, second being line content
+  # default indent pattern is tabs
+  def text2array(indent_pattern = /^(\t*)/)
+    lines = Array.new
 
-    # if first level, grab ckey or empty out
-    if level == 0
-      ckey = (ckey_pattern =~ line) ? $1 : ''
+    @text.dup.each_line_with_index do |l, i|
+
+      # count tabs at front of line to get indent level
+      tabs = $1 if indent_pattern =~ l
+      level = (defined? tabs) ? tabs.size : 0
+
+      lines[i] = {:level => level, :line => l}
     end
 
-    if i == lines.size-1 || lines[i+1][:level] <= level   # if it's the last entry, or the next entry is lower level
-      lcontext[i] = {:context => line.strip, :ckey => ckey}
-    else
-      c = i + 1         # set start of counter to current pub
+    @lines = lines
+  end
 
-      # as long as the level of the line in question is higher than the start level
-      while c < lines.size && lines[c][:level] > level
-        text << lines[c][:line][level..-1] # remove the same number of indents as highest level has, preserve subsequent indents
-        c += 1
+
+  # iterate through array and for each line, pick out "line context" (all subsequent lines at lower levels)
+  def linecontext(ckey_pattern = /^\[!!!!(.+?)\]/)
+    ckey = ''       # holds the current citation key
+    lcontext = Array.new
+    text = ''
+
+    @lines.dup.each_with_index do |l, i|
+      level = l[:level]
+      line = l[:line]
+
+      # if first level, grab ckey or empty out
+      if level == 0
+        ckey = (ckey_pattern =~ line) ? $1 : ''
       end
 
-      lcontext[i] = {:context => line + text, :ckey => ckey}
-      text = ''
-    end
-  end
-  return lcontext
-end
+      if i == @lines.size-1 || @lines[i+1][:level] <= level   # if it's the last entry, or the next entry is lower level
+        lcontext[i] = {:context => line.strip, :ckey => ckey}
+      else
+        c = i + 1         # set start of counter to current pub
 
+        # as long as the level of the line in question is higher than the start level
+        while c < @lines.size && @lines[c][:level] > level
+          text << @lines[c][:line][level..-1] # remove the same number of indents as highest level has, preserve subsequent indents
+          c += 1
+        end
 
-# process tags, taking line context and ckey into account
-def process_tags(lines, lcontext, tag_regexp = /\s\@(.+?)\b/, ckey_pattern = /^\[!!!!(.+?)\]/)
-  all_tagged = '' # holds all tagged text, to later check for lines that have not been tagged
-  tags = Hash.new
-
-  # iterate through array, and if tag is found, insert line context for that line into tag hash
-  lines.each_with_index do |line, i|
-    hastag = line[:line].scan2(tag_regexp)        # recognize a @tag
-    if hastag
-      hastag[:tagcapt].each do |x|            # for each tag if multiple
-        cont = lcontext[i][:context]                 # get the context for the current line
-        cont.remove!(/\[!!!!#{lcontext[i][:ckey]}\] /, tag_regexp, /\:$/)
-        tags.add(x, [cont.strip, lcontext[i][:ckey]])
-        all_tagged << cont
+        lcontext[i] = {:context => line + text, :ckey => ckey}
+        text = ''
       end
     end
+    @lcontext = lcontext
   end
 
-  # do a final sweep to see if any lines have not been collected
-  lines.each_with_index do |l, i|
-    next if l[:line].remove(ckey_pattern).strip.size == 0       # nothing but ckey
-    next if l[:line].scan2(tag_regexp)                          # recognize a @tag
 
-    cont = l[:line].remove(/\[\@#{lcontext[i][1]}\]/, /\:$/).strip
+  # process tags, taking line context and ckey into account
+  def process_tags(tag_regexp = /\s\@(.+?)\b/, ckey_pattern = /^\[!!!!(.+?)\]/)
+    all_tagged = '' # holds all tagged text, to later check for lines that have not been tagged
+    tags = Hash.new
 
-    unless all_tagged.index(cont) # unless it has been tagged
-      cont.remove!(/\[!!!!#{lcontext[i][:ckey]}\] /, tag_regexp, /\:$/)
-      tags.add('not_tagged', [cont, lcontext[i][:ckey]])
-    end
-  end
-  return tags
-end
-
-
-# format output for taskpaper
-def output_taskpaper(tags)
-   out = ''
-
-   tags.each do |tag, content|
-    nockey = ''
-
-    out << "#{tag}:\n"
-    content.each do |fragments|
-      fragments[0] = fragments[0].lines.map {|ln| ("\t\t" + ln).remove("\n")}
-
-      if fragments[1] == ''
-        nockey << "#{fragments[0].join("\n")}\n"
-        next
+    # iterate through array, and if tag is found, insert line context for that line into tag hash
+    @lines.dup.each_with_index do |line, i|
+      hastag = line[:line].scan2(tag_regexp)        # recognize a @tag
+      if hastag
+        hastag[:tagcapt].each do |x|            # for each tag if multiple
+          cont = @lcontext[i][:context]                 # get the context for the current line
+          cont.remove!(/\[!!!!#{@lcontext[i][:ckey]}\] /, tag_regexp, /\:$/)
+          tags.add(x, [cont.strip, @lcontext[i][:ckey]])
+          all_tagged << cont
+        end
       end
-
-      out << "\t[@#{fragments[1]}]:\n#{fragments[0].join("\n")}\n"
     end
-    out << "\tNo citekey:\n#{nockey}" if nockey.size > 0
-  end
-  return out
-end
 
+    # do a final sweep to see if any lines have not been collected
+    @lines.dup.each_with_index do |l, i|
+      next if l[:line].remove(ckey_pattern).strip.size == 0       # nothing but ckey
+      next if l[:line].scan2(tag_regexp)                          # recognize a @tag
 
-# format output for scrivener
-def output_scrivener(tags)
-  outdir = "#{Time.now.to_i.to_s}.tmp"
-  `mkdir '#{outdir}'`
-  tags.each do |tag, content|
-    out = ''
-    nockey = ''
+      cont = l[:line].remove(/\[\@#{@lcontext[i][1]}\]/, /\:$/).strip
 
-    tag = restore_ckeys(tag)
-    content.each do |fragments|
-      fragments[0] = restore_ckeys(fragments[0])
-      fragments[1] = restore_ckeys(fragments[1])
-
-      if fragments[1] == ''
-        nockey << "#{fragments[0]}\n\n"
-        next
+      unless all_tagged.index(cont) # unless it has been tagged
+        cont.remove!(/\[!!!!#{@lcontext[i][:ckey]}\] /, tag_regexp, /\:$/)
+        tags.add('not_tagged', [cont, @lcontext[i][:ckey]])
       end
-      out << "#{fragments[0]} [@#{fragments[1]}]\n\n"
     end
-    if nockey.size > 0
-      out << nockey
-    end
-    File.write("#{outdir}/#{tag}.txt", out)
+    @tags = tags
   end
-  return outdir
+
+
+  # format output for taskpaper
+  def output_taskpaper
+     out = ''
+
+     @tags.dup.each do |tag, content|
+      nockey = ''
+
+      out << "#{tag}:\n"
+      content.each do |fragments|
+        fragments[0] = fragments[0].lines.map {|ln| ("\t\t" + ln).remove("\n")}
+
+        if fragments[1] == ''
+          nockey << "#{fragments[0].join("\n")}\n"
+          next
+        end
+
+        out << "\t[@#{fragments[1]}]:\n#{fragments[0].join("\n")}\n"
+      end
+      out << "\tNo citekey:\n#{nockey}" if nockey.size > 0
+    end
+    return out
+  end
+
+
+  # format output for scrivener
+  def to_scrivener(dir)
+    outdir = "#{Time.now.to_i.to_s}.tmp"
+    `mkdir '#{dir}'`
+    @tags.dup.each do |tag, content|
+      out = ''
+      nockey = ''
+
+      tag = restore_ckeys(tag)
+      content.each do |fragments|
+        fragments[0] = restore_ckeys(fragments[0])
+        fragments[1] = restore_ckeys(fragments[1])
+
+        if fragments[1] == ''
+          nockey << "#{fragments[0]}\n\n"
+          next
+        end
+        out << "#{fragments[0]} [@#{fragments[1]}]\n\n"
+      end
+      if nockey.size > 0
+        out << nockey
+      end
+      File.write("#{outdir}/#{tag}.txt", out)
+    end
+    return outdir
+  end
 end
 
-a = File.read("Litreview.taskpaper")
+if __FILE__ == $0
 
-text = clean_ckeys(a)
-lines = text2array(text)
-lcontext = linecontext(lines)
-tags = process_tags(lines, lcontext)
+  a = File.read("Litreview.taskpaper")
 
-puts output_scrivener(tags.dup)
+  text = clean_ckeys(a)
+  lines = text2array(text)
+  lcontext = linecontext(lines)
+  tags = process_tags(lines, lcontext)
 
-output = output_taskpaper(tags.dup)
-output = restore_ckeys(output)
+  puts output_scrivener(tags.dup)
 
-File.write('out.taskpaper', output)
-exit
+  output = output_taskpaper(tags.dup)
+  output = restore_ckeys(output)
 
+  File.write('out.taskpaper', output)
+  exit
 
+end
 
 
 
